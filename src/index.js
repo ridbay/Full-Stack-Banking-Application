f8f1d884cb25beb29a0c299d82deffc820ad5edf
Ridwan Balogun <balogunridwan@gmail.com>
2020-11-09 21:18:58 +0100

    Connected store with the approuter in the root folder

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+// import App from "./App";
+import AppRouter from "./router/AppRouter";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/main.scss";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>,
+
+  document.getElementById("root")
+);
